757a5f842f5fbd5a04ef8740b391a6270ac15f58
Bertrand Thomas <bertrand.thomas@suse.com>
2022-08-12 09:30:30 +0000

    Feature/get system info

---
+++ src/WebApi/Controllers/SystemInformationController.cs
@@ -0,0 +1,22 @@
+﻿using Ecorp.BackendDemo.WebApi.Dto;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Ecorp.BackendDemo.WebApi.Controllers
+{
+    [ApiController]
+    [Route("system-informations")]
+    public class SystemInformationController : ControllerBase
+    {
+        [HttpGet(Name = "GetSystemInformation")]
+        public SystemInformationDto Toto()
+        {
+            return new SystemInformationDto
+            {
+                MachineName = Environment.MachineName,
+                ProcessorArchitecture = System.Runtime.InteropServices.RuntimeInformation.ProcessArchitecture.ToString(),
+                FrameworkDescription = System.Runtime.InteropServices.RuntimeInformation.FrameworkDescription,
+                RuntimeIdentifier = System.Runtime.InteropServices.RuntimeInformation.RuntimeIdentifier
+            };
+        }
+    }
+}
---
+++ src/WebApi/Dto/SystemInformationDto.cs
@@ -0,0 +1,12 @@
+﻿using System.Runtime.InteropServices;
+
+namespace Ecorp.BackendDemo.WebApi.Dto
+{
+    public class SystemInformationDto
+    {
+        public string MachineName { get; set; } = string.Empty;
+        public string ProcessorArchitecture { get; internal set; } = string.Empty;
+        public string FrameworkDescription { get; internal set; } = string.Empty;
+        public string RuntimeIdentifier { get; internal set; } = string.Empty;
+    }
+}
